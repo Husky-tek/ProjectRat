35be4792ae3608b3d1f4b0cdf03a380ab2cc7610
Husky-tek <huskytecks@gmail.com>
2025-05-28 15:38:44 -0600

    added enemyChar

---
+++ projecta/scripts/enemychar.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+
+enum States {IDLE,WALKING,INAIR,DAMAGED,FASTFALL,CROUCH,DODGE}
+enum lookat {RIGHT,LEFT}
+enum action {IDLE, ATTACK, SPECIAL, MOVING}
+
+var HP = 10
+var Attack = 0.0
+var speed = 1
+
+var currState = States.IDLE
+var look = lookat.RIGHT
+var curAction = action.IDLE
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		currState = States.INAIR
+	else:
+		currState = States.WALKING
+
+func take_damage(damage):
+	currState = States.DAMAGED
